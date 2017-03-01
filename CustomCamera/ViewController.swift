49421d6e8260981db44ee9c014e258aa0f4319ed
ShanghaiTimes <racuk12@gmail.com>
2017-03-01 12:30:01 +0000

    Updates

--- CustomCamera/ViewController.swift
+++ CustomCamera/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 import AVFoundation
 import Foundation
 
+class Ring:UIView {
+    func drawRect(rect: CGRect)
+    {
+        drawRingFittingInsideView()
+    }
+    internal func drawRingFittingInsideView()->()
+    {
+        let halfSize:CGFloat = min( bounds.size.width/2, bounds.size.height/2)
+        let desiredLineWidth:CGFloat = 1    // your desired value
+        
+        let circlePath = UIBezierPath(
+            arcCenter: CGPoint(x:halfSize,y:halfSize),
+            radius: CGFloat( halfSize - (desiredLineWidth/2) ),
+            startAngle: CGFloat(0),
+            endAngle:CGFloat(M_PI * 2),
+            clockwise: true)
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = circlePath.cgPath
+        
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = UIColor.blue.cgColor
+        shapeLayer.lineWidth = desiredLineWidth
+        
+        layer.addSublayer(shapeLayer)
+    }
+
+    
+
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var navigationBar: UINavigationBar!
@@ -20,13 +51,16 @@ class ViewController: UIViewController {
     let stillImageOutput = AVCaptureStillImageOutput()
     var previewLayer : AVCaptureVideoPreviewLayer?
     
+ 
+    
     // If we find a device we'll store it here for later use
     var captureDevice : AVCaptureDevice?
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
         captureSession.sessionPreset = AVCaptureSessionPresetHigh
         
         if let devices = AVCaptureDevice.devices() as? [AVCaptureDevice] {
@@ -35,7 +69,7 @@ class ViewController: UIViewController {
                 // Make sure this particular device supports video
                 if (device.hasMediaType(AVMediaTypeVideo)) {
                     // Finally check the position and confirm we've got the back camera
-                    if(device.position == AVCaptureDevicePosition.front) {
+                    if(device.position == AVCaptureDevicePosition.back) {
                         captureDevice = device
                         if captureDevice != nil {
                             print("Capture device found")
@@ -80,18 +114,21 @@ class ViewController: UIViewController {
         self.view.addSubview(navigationBar)
         self.view.addSubview(imgOverlay)
         self.view.addSubview(btnCapture)
-    }
-    
+       
+        
+            }
+
     func saveToCamera() {
         
         if let videoConnection = stillImageOutput.connection(withMediaType: AVMediaTypeVideo) {
-            
-            stillImageOutput.captureStillImageAsynchronously(from: videoConnection, completionHandler: { (CMSampleBuffer, Error) in
+              stillImageOutput.captureStillImageAsynchronously(from: videoConnection, completionHandler: { (CMSampleBuffer, Error) in
+                
                 if let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(CMSampleBuffer) {
-                    
                     if let cameraImage = UIImage(data: imageData) {
+
                         
-                        UIImageWriteToSavedPhotosAlbum(cameraImage, nil, nil, nil)                        
+                       UIImageWriteToSavedPhotosAlbum(cameraImage, nil, nil, nil)
+
                     }
                 }
             })
@@ -102,5 +139,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 }
 
